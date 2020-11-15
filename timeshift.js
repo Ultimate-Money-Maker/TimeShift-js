503df3eed9cff74f6f65c31aad848a08de8e54ec
eyal <mail@eyal-matalon.com>
2020-11-15 17:29:39 +0200

    Added (GMT Standard Time) label to timezone

--- timeshift.js
+++ timeshift.js
@@ -198,7 +198,7 @@
 		var d = utcToLocal(this.utc);
 		// Mon Mar 05 2012 06:07:08 GMT+0500
 		return wkdays[d.getUTCDay()] + " " + months[d.getUTCMonth()] + " " + twoDigit(d.getUTCDate()) + " " + d.getUTCFullYear() +
-			" " + twoDigit(d.getUTCHours()) + ":" + twoDigit(d.getUTCMinutes()) + ":" + twoDigit(d.getUTCSeconds()) + " " + timezoneName();
+			" " + twoDigit(d.getUTCHours()) + ":" + twoDigit(d.getUTCMinutes()) + ":" + twoDigit(d.getUTCSeconds()) + " " + timezoneName() + " (GMT Standard Time)";
 	}
 	TimeShift.Date.prototype.toLocaleString = function() { return this.toString(); }  // Wrong
 	TimeShift.Date.prototype.toLocaleTimeString = function() { return this.toString(); }  // Wrong
